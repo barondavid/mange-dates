3673682830e2f6bf448747a6800ba0754e626504
David Barón <baron.david17@gmail.com>
2020-04-24 02:53:55 -0300

    Add form into form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,45 @@ import React, { Fragment } from 'react';
 const Form = () => {
   return (
     <Fragment>
-      <h1>Form</h1>
+      <h2>create date</h2>
+      <form>
+        <label>Pet name</label>
+        <input
+          type="text" 
+          name="pet"
+          className="u-full-width"
+          placeholder="Pet name"
+        />
+        <label>Owner</label>
+        <input
+          type="text" 
+          name="owner"
+          className="u-full-width"
+          placeholder="Owner name"
+        />
+        <label>Date</label>
+        <input
+          type="date" 
+          name="date"
+          className="u-full-width"
+        />
+        <label>Hour</label>
+        <input
+          type="time" 
+          name="time"
+          className="u-full-width"
+        />
+        <label>Symptom</label>
+        <textarea
+          className="u-full-width"
+          name="symptom"
+        >
+        </textarea>
+        <button
+          type="submit"
+          className="u-full-width button-primary"
+        >Add Date</button>
+      </form>
     </Fragment>
   );
 }
